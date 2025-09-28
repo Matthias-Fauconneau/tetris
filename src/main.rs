3a35793ae0c20bf24c926fc96011ac5dc74bf283
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2025-09-28 15:35:32 +0200

    fall

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,49 @@
 #![feature(slice_from_ptr_range)] // shader
-use ui::{Result, run, new_trigger, size, int2, image::{Image, xy, rgb8}, Widget, vulkan, shader};
+use ui::{Result, run, size, int2, image::{Image, xy, rgb8}, Widget, vulkan, shader};
 use vulkan::{Context, Commands, Arc, ImageView, PrimitiveTopology, image, WriteDescriptorSet, linear};
 shader!{view}
 
 struct App {
-	state: Image<Box<[u8]>>
+	state: Image<Box<[u8]>>,
+	ops: usize,
+	steps: usize,
+	done: bool
 }
 
 impl App {
 	fn new() -> Self { 
-		let size = xy{x: 1280, y: 720};
+		let size = xy{x: 80, y: 45};
 		let mut state = Image::zero(size);
-		state[xy{x: 1*size.x/3, y: size.y/2}] = 1;
-		state[xy{x: 2*size.x/3, y: size.y/2}] = 2;
-		Self{state}
+		state[xy{x: size.x/2, y: size.y/2}] = 255;
+		Self{state, ops: 0, steps: 0, done: false}
 	}
 }
 
 impl Widget for App { 
-fn event(&mut self, _: &Context, _: &mut Commands, _: size, _: &mut ui::EventContext, event: &ui::Event) -> Result<bool> { Ok(matches!(event, ui::Event::Idle)) }
+fn event(&mut self, _: &Context, _: &mut Commands, _: size, _: &mut ui::EventContext, event: &ui::Event) -> Result<bool> { Ok(!self.done && matches!(event, ui::Event::Idle)) }
 fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context, commands: &mut Commands, target: Arc<ImageView>, _: size, _: int2) -> Result {
 	use rand::random_range;
-	let Self{state} = self;
+	let Self{state, ops, steps, done} = self;
+	let mut op = 0;
 	for _ in 0..1024 {
-		loop {
-			let p0@xy{x,y} = xy{x: random_range(1..state.size.x-1), y: random_range(1..state.size.y-1)};
-			let p1 = match random_range(0..4) {
-				0 => xy{x: x-1, y},
-				1 => xy{x: x+1, y},
-				2 => xy{x, y: y-1},
-				3 => xy{x, y: y+1},
-				_ => unreachable!()
-			};
-			if state[p0] == 1 && state[p1] == 0 { state[p1] = 1; break; }
-			if state[p0] == 2 && state[p1] == 0 { state[p1] = 2; break; }
-			if state[p0] == 1 && state[p1] == 2 { state[p0] = 3; state[p1] = 3; break; }
-		}
+		let p0@xy{x,y} = xy{x: random_range(1..state.size.x-1), y: random_range(1..state.size.y-1)};
+		if state[p0] == 0 { continue; } // fast-path // TODO: random_range in non-zero
+		let p1 = match random_range(0..4) {
+			0 => xy{x: x-1, y},
+			1 => xy{x: x+1, y},
+			2 => xy{x, y: y-1},
+			3 => xy{x, y: y+1},
+			_ => unreachable!()
+		};
+		if state[p0] > state[p1]+1 { state[p0] -= 1; state[p1] += 1; op+=1; break; }
 	}
+	*ops += op; *steps += 1;
+	//println!("{ops} {steps}"); 
+	if *ops >= 978 || *steps >= 1431 { *done = true; }
 	let mut pass = view::Pass::new(context, false, PrimitiveTopology::TriangleList, false)?;
-	let image = image(context, commands, state.as_ref().map(|&s| [rgb8{r:0,g:0,b:0},rgb8{r:0xFF,g:0xFF,b:0},rgb8{r:0,g:0xFF,b:0xFF},rgb8{r:0,g:0xFF,b:0}][s as usize].into()).as_ref())?;
+	let image = image(context, commands, state.as_ref().map(|&s| 
+		//if s < 4 { [rgb8{r:0,g:0,b:0},rgb8{r:0xFF,g:0xFF,b:0},rgb8{r:0,g:0xFF,b:0xFF},rgb8{r:0,g:0xFF,b:0}][s as usize] } else { rgb8{r:s,g:s,b:s} }
+		{let g = s.min(15)*16; rgb8{r:g,g:g,b:g}}.into()).as_ref())?;
 	pass.begin_rendering(context, commands, target.clone(), None, true, &view::Uniforms::empty(), &[
 		WriteDescriptorSet::image_view(0, ImageView::new_default(&image)?),
 		WriteDescriptorSet::sampler(1, linear(context)),
@@ -49,4 +54,4 @@ fn paint(&mut self, context/*@Context{device, memory_allocator, ..}*/: &Context,
 }
 }
 
-fn main() -> Result { run(new_trigger().unwrap(), "view", Box::new(|_,_| Ok(Box::new(App::new())))) }
+fn main() -> Result { run("toy", Box::new(|_,_| Ok(Box::new(App::new())))) }
